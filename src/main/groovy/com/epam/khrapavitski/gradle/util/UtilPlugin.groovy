282eefd7c9667517e03f4b80a482ba7ee5cd6069
Raman Khrapavitski <Raman_Khrapavitski@epam.com>
2016-06-24 16:07:59 +0300

    task sort gradle.properties added to plugin

--- src/main/groovy/com/epam/khrapavitski/gradle/util/UtilPlugin.groovy
+++ src/main/groovy/com/epam/khrapavitski/gradle/util/UtilPlugin.groovy
@@ -4,6 +4,22 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class UtilPlugin implements Plugin<Project>{
-    void apply(Project target) {
+    
+    void apply(Project project) {
+        applyTasks(project)
     }
+    
+    void applyTasks(Project project) {
+        [
+            'sortProperties': 'Sort file gradle.properties'
+        ].each { taskName, taskDescription ->
+            def commandName = taskName
+            project.task(taskName, type: UtilTask) {
+                description = taskDescription
+                command = commandName
+                group = "util"
+            }
+        }
+    }
+    
 }
--- src/main/groovy/com/epam/khrapavitski/gradle/util/UtilTask.groovy
+++ src/main/groovy/com/epam/khrapavitski/gradle/util/UtilTask.groovy
@@ -1,7 +1,44 @@
 package com.epam.khrapavitski.gradle.util
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
 
 class UtilTask extends DefaultTask {
 
+    def command
+
+    @TaskAction
+    def utilAction(){
+        println "gradle-project-util: Running 'util $command'"
+        project.logger.debug("gradle-project-util: Running 'util $command'")
+        switch (command) {
+            case ~/\b(S|s)ort(Properties|properties|Props|props)?\b/:
+                sortProperties()
+                break
+            default:
+                println "gradle-project-util: Unkown command"
+                break   
+        }
+    }
+
+    def sortProperties(){
+        def file = project.file("${project.projectDir}/gradle.properties")
+        if(!file){
+            println "gradle-project-util: File gradle.properties is not found"
+            project.logger.debug("gradle-project-util: File gradle.properties is not found")
+        }else{
+            def properties = new Properties()
+            properties.load(file.newDataInputStream())
+            if(properties.size() == 1 || properties.isEmpty()){
+                println "gradle-project-util: Nothing to sort"
+            }else{
+                properties = properties.sort{a,b -> a.getKey() <=> b.getKey()}
+                println "gradle-project-util: File gradle.properties sorted succsessfuly"
+                println "gradle-project-util: Sorted list of properties:"
+                properties.each{println it}
+                file.withWriter {out -> properties.each {out.println it}}
+            }
+        }
+    }
+    
 }
